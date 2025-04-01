89975f3994a78de673f7120e8cf0c3f6814fab41
Raul <raul_cosmin_26@yahoo.com>
2025-04-01 08:20:52 +0300

    Create basic nodejs server.

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("Hello, world");
+import { createServer } from "node:http";
+
+const server = createServer((req, res) => {
+  console.log(req.method);
+  res.end("Hello World!");
+});
+
+server.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
