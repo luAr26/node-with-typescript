c319b39719b194d19d2afc0018923daa6079481f
Raul <raul_cosmin_26@yahoo.com>
2025-04-02 08:39:31 +0300

    Add the necessary changes to to run typescript files without transpile.

--- app.ts
+++ app.ts
@@ -1,5 +1,9 @@
-import express, { NextFunction, Request, Response } from "express";
-import todoRoutes from "./routes/todo.js";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import todoRoutes from "./routes/todo.ts";
 
 const app = express();
 app.use(express.json());
@@ -12,3 +16,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
+
+// To run typescript   without the need to transpile run the following command:
+// node --experimental-strip-types app.ts
