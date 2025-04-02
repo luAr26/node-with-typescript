3662f196fb8fe605af8ae1f71a5b963560497f6f
Raul <raul_cosmin_26@yahoo.com>
2025-04-02 08:27:50 +0300

    Add all routes for the basic CRUD operations.

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import todoRoutes from "./routes/todo.js";
 
 const app = express();
 app.use(express.json());
 
 app.use(todoRoutes);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: "An error occurred!" });
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
--- routes/todo.ts
+++ routes/todo.ts
@@ -1,18 +1,37 @@
 import express from "express";
-import { addTodo } from "../data.js";
+import { addTodo, getTodo, getTodos, removeTodo, updateTodo } from "../data.js";
 
 const router = express.Router();
 
 // function handlePostTodos(req: Request, res: Response) {}
 
+router.get("/todos", (req, res) => {
+  const todos = getTodos();
+  res.json({ todos });
+});
+
+router.get("/todos/:id", (req, res) => {
+  const todo = getTodo(+req.params.id);
+  res.json({ todo });
+});
+
 router.post("/todos", (req, res) => {
-  console.log("Request body", req.body);
   const text = req.body.text;
   if (!text) {
     res.status(400).json({ error: "Text field is required..." });
   }
   const addedTodo = addTodo(text);
-  res.json({ message: "Todo added", todo: addedTodo });
+  res.json({ message: "Todo added.", todo: addedTodo });
+});
+
+router.patch("/todos/:id", (req, res) => {
+  const updatedTodo = updateTodo(+req.params.id, req.body.text);
+  res.json({ message: "Todo updated.", todo: updatedTodo });
+});
+
+router.delete("/todos/:id", (req, res) => {
+  removeTodo(+req.params.id);
+  res.json({ message: "Todo deleted." });
 });
 
 export default router;
